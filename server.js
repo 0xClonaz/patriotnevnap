19f95d699599ef7b167dafa82337cd379b14f44d
0xClonaz <136977848+0xClonaz@users.noreply.github.com>
2024-05-01 23:45:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/nevnap", async (req, res) => {
+app.get("/patriotnevnap", async (req, res) => {
     try {
         // Az API URL-jét a sajátodra cseréld
         const apiResponse = await axios.get('https://api.nevnapok.eu/ma');
         const data = apiResponse.data;
         // Feltételezem, hogy az adatok a JSON válasz egy bizonyos kulcs alatt vannak
         const names = data[Object.keys(data)[0]].join(', ');
-        res.send(`Mai névnapok: ${names}`);
+        res.send(`Patriot megmondja neked a mai névnapokat teljesen ingyen: ${names}`);
     } catch (error) {
         console.error('API Error:', error);
         res.send("Hiba történt a névnapok lekérésekor.");
